568c3e2d8dfa16c1a50c4352625bf789cb366fea
Ivan Lisovskii <dnevanek@gmail.com>
2023-11-08 13:24:48 +0300

    anime page first look

--- pages/anime.vue
+++ pages/anime.vue
@@ -1,6 +1,18 @@
 <template>
-    <Navigation />
-    <div>
-        anime page
+    <div class="wrap bg-dark">
+        <Navigation/>
+        <div class="main-content">
+        <AnimeCardContainer />
+        </div>
     </div>
 </template>
+
+<style scoped>
+.wrap {
+    height: auto;
+    width: 100%;
+}
+.main-content {
+    margin-top: 50px;
+}
+</style>
