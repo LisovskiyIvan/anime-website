1982dbca91bcd2e5e3a86d1dde272e07de1adbd9
Lisovskii Ivan <dnevanek@gmail.com>
2024-01-13 21:55:10 +0300

    started filters

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@ useHead({
   "link": [
     { "rel": "stylesheet", "href": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" }
   ],
-  title: 'Аниме вебсайт'
+  title: 'Aniziga'
 }
 )
 
--- components/Filters.vue
+++ components/Filters.vue
@@ -1,12 +1,29 @@
 <template>
-    <div class="filters card textFont">
-        Here will besome filters
+    <div class="filters card textFont h-[600px] bg-gray-200">
+        <h1 class="m-4 font-bold text-2xl">Фильтры</h1>
+        <div class="w-[80%] mx-[10%] mb-[10%]">
+            <div class="p-[5px] bg-white">
+                <MultiSelect v-model="selectedCities" :options="cities" optionLabel="name" placeholder="Select genres"
+                    :maxSelectedLabels="10" class="w-full md:w-20rem" />
+            </div>
+        </div>
     </div>
 </template>
 
+<script setup>
+const selectedCities = ref();
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
+
+</script>
+
 <style scoped>
 .filters {
-    height: fit-content;
     max-height: 800px;
     width: 25%;
     max-width: 500px;
